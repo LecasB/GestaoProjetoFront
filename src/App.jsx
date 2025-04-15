7012824b222e20fc6aa6e2a0f232939f97b1bb01
Leonardo <cleonardo191@gmail.com>
2025-04-15 01:24:05 +0100

    ola mundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import xuoLogo from "./assets/xuo.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={xuoLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Best Market Place 🚫 🧢</h1>
+      <a href="https://xuoapi.vercel.app/">
+        <marquee behavior="scroll" direction="left" scrollamount="10">
+          Aceda a API aqui!
+        </marquee>
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
