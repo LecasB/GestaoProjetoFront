9371d126f5ac7ef2887115a17c583dd689b81c68
Carlos <cd121433@gmail.com>
2025-05-12 22:24:15 +0100

    Wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import xuoLogo from "./assets/xuo.png";
 import "./App.css";
+import Footer from "./molecules/footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
-      <div>
-        <img src={xuoLogo} className="logo" alt="Vite logo" />
-      </div>
-      <h1>Best Market Place 🚫 🧢</h1>
-      <p>OH YEAH MAMAS</p>
-      <a href="https://xuoapi.vercel.app/">
-        <marquee behavior="scroll" direction="left" scrollamount="10">
-          Aceda a API aqui!
-        </marquee>
-      </a>
+    <Footer>
+      
+    </Footer>
+
     </>
   );
 }
