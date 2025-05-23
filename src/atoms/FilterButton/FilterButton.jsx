345c71dc13ba76a12a493085727ffe51beb60b88
Tiago Nogueira <tiagoandresn@gmail.com>
2025-05-23 23:32:04 +0100

    wip

--- src/atoms/FilterButton/FilterButton.jsx
+++ src/atoms/FilterButton/FilterButton.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './FilterButton.scss';
+import { useState, useRef, useEffect } from "react";
+import "./FilterButton.scss";
 
 const FilterButton = ({ onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef(null);
 
-  const toggleMenu = () => setIsOpen(prev => !prev);
+  const toggleMenu = () => setIsOpen((prev) => !prev);
 
   const handleOptionClick = (option) => {
     setIsOpen(false);
@@ -18,14 +18,17 @@ const FilterButton = ({ onSelect }) => {
         setIsOpen(false);
       }
     };
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   return (
     <div className="filter-container" ref={buttonRef}>
-      <button className={`filter-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        FILTER
+      <button
+        className={`filter-button ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span> FILTER </span>
         <svg
           className="arrow-icon"
           xmlns="http://www.w3.org/2000/svg"
@@ -38,15 +41,15 @@ const FilterButton = ({ onSelect }) => {
           strokeLinecap="round"
           strokeLinejoin="round"
         >
-          <polyline points={isOpen ? '6 15 12 9 18 15' : '6 9 12 15 18 9'} />
+          <polyline points={isOpen ? "6 15 12 9 18 15" : "6 9 12 15 18 9"} />
         </svg>
       </button>
 
       {isOpen && (
         <ul className="filter-dropdown">
-          <li onClick={() => handleOptionClick('VENDIDOS')}>VENDIDOS</li>
-          <li onClick={() => handleOptionClick('COMPRADOS')}>COMPRADOS</li>
-          <li onClick={() => handleOptionClick('WISHLIST')}>WISHLIST</li>
+          <li onClick={() => handleOptionClick("VENDIDOS")}>VENDIDOS</li>
+          <li onClick={() => handleOptionClick("COMPRADOS")}>COMPRADOS</li>
+          <li onClick={() => handleOptionClick("WISHLIST")}>WISHLIST</li>
         </ul>
       )}
     </div>
