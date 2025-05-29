ecde27b7a983348207f396bf39d9d97cb9709114
Leonardo <cleonardo191@gmail.com>
2025-05-29 22:23:33 +0100

    wd

--- src/organisms/ProfilePage/ProfilePage.jsx
+++ src/organisms/ProfilePage/ProfilePage.jsx
@@ -26,11 +26,13 @@ const ProfilePage = () => {
   const [filterValue, setFilterValue] = useState("All Items");
   const [info, setInfo] = useState("followers");
   const [showPopupUser, setShowPopupUser] = useState(false);
+  const [follow, setFollow] = useState("");
+  const [following, setFollowing] = useState("");
+  const [isFollowing, setIsFollowing] = useState("");
 
   const fileUploadRef = useRef(null);
 
   const updateProfile = async () => {
-
     try {
       if (username != userDetails.username) {
         const checkResponse = await fetch(
@@ -124,6 +126,42 @@ const ProfilePage = () => {
       });
   };
 
+  const getFollowInfo = async (id = sessionStorage.getItem("id")) => {
+    await fetch(`https://xuoapi.azurewebsites.net/api/v1/follow/status/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFollow(data.followers);
+        setFollowing(data.following);
+      })
+      .catch((error) => {
+        console.error("Error fetching item details:", error);
+      });
+  };
+
+  const handleVerifyFollow = async (id) => {
+    try {
+      const response = await fetch(
+        `https://xuoapi.azurewebsites.net/api/v1/follow/verify`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            idfollower: sessionStorage.getItem("id"),
+            idfollowed: id,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      setIsFollowing(data.isFollowing);
+    } catch (error) {
+      console.error("Error fetching item details:", error);
+    }
+  };
+
   const getUserInfo = async (id = sessionStorage.getItem("id")) => {
     await fetch(`https://xuoapi.azurewebsites.net/api/v1/user/${id}`)
       .then((response) => response.json())
@@ -156,7 +194,38 @@ const ProfilePage = () => {
 
   const handleClose = () => {
     setShowPopupUser(false);
-  }
+  };
+
+  const changeFollowState = async (state) => {
+    const endpoint =
+      state === "follow"
+        ? "https://xuoapi.azurewebsites.net/api/v1/follow/new"
+        : "https://xuoapi.azurewebsites.net/api/v1/follow/delete";
+
+    // Define o método conforme o estado
+    const method = state === "follow" ? "POST" : "DELETE";
+
+    try {
+      const response = await fetch(endpoint, {
+        method: method, // ou apenas `method,` ES6 shorthand
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idfollower: sessionStorage.getItem("id"),
+          idfollowed: userDetails._id,
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Follow response:", data);
+
+      // Atualizar estado com base na ação
+      setIsFollowing(state === "follow");
+    } catch (error) {
+      console.error("Erro ao alterar o estado de follow:", error);
+    }
+  };
 
   const renderStars = () => {
     const stars = [];
@@ -190,6 +259,8 @@ const ProfilePage = () => {
 
   useEffect(() => {
     id ? getUserInfo(id) : getUserInfo();
+    id ? getFollowInfo(id) : getFollowInfo();
+    handleVerifyFollow(id);
   }, []);
 
   useEffect(() => {
@@ -198,7 +269,9 @@ const ProfilePage = () => {
 
   return (
     <div>
-      {showPopupUser && <PopupUserInfo info={info} id={userDetails._id} close={handleClose} />}
+      {showPopupUser && (
+        <PopupUserInfo info={info} id={userDetails._id} close={handleClose} />
+      )}
       <Dialog
         ola="aaa"
         header="Editar Perfil"
@@ -252,7 +325,14 @@ const ProfilePage = () => {
           <div className="cmp-profile-page__info__top">
             <div className="cmp-profile-page__info__top__userNstars">
               <h1>@{userDetails.username}</h1>
-              <p style={{ fontSize: "20px", display: "flex", alignItems: "center", gap: "5px" }}>
+              <p
+                style={{
+                  fontSize: "20px",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                }}
+              >
                 {`(${userRate.averageRate})`}
                 <span style={{ paddingLeft: "5px" }}>{renderStars()}</span>
                 <span
@@ -264,9 +344,7 @@ const ProfilePage = () => {
                 >
                   ({userRate.totalReviews}) Reviews
                 </span>
-
               </p>
-
             </div>
             {userDetails._id === sessionStorage.getItem("id") ? (
               <div
@@ -280,17 +358,49 @@ const ProfilePage = () => {
               </div>
             ) : (
               <div>
-                <Button label="Follow"></Button>
+                {isFollowing ? (
+                  <Button
+                    style={{
+                      background: "white",
+                      border: "1px solid #06b6d4",
+                      color: "#06b6d4",
+                    }}
+                    onClick={() => {
+                      changeFollowState("unfollow");
+                    }}
+                    label="Unfollow"
+                  ></Button>
+                ) : (
+                  <Button
+                    onClick={() => {
+                      changeFollowState("follow");
+                    }}
+                    label="Follow"
+                  ></Button>
+                )}
               </div>
             )}
           </div>
 
           <div className="cmp-profile-page__info__middle">
-            <p style={{ cursor: "pointer" }} onClick={() => { setInfo("following"), setShowPopupUser(!showPopupUser) }}>Following: 4</p>
-            <p style={{ cursor: "pointer" }} onClick={() => { setInfo("followers"), setShowPopupUser(!showPopupUser) }}>Followers: 5</p>
+            <p
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setInfo("following"), setShowPopupUser(!showPopupUser);
+              }}
+            >
+              Following: {following.length}
+            </p>
+            <p
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setInfo("followers"), setShowPopupUser(!showPopupUser);
+              }}
+            >
+              Followers: {follow.length}
+            </p>
             <p>Items: {numberItems}</p>
           </div>
-
 
           <p className="cmp-profile-page__info__description">
             Description: {userDetails.descricao}
@@ -311,7 +421,9 @@ const ProfilePage = () => {
           }}
         >
           <h1 style={{ padding: "0px 0px 0px 20px" }}>Items</h1>
-          {userDetails._id == sessionStorage.getItem("id") && <FilterButton onSelect={setFilterValue} />}
+          {userDetails._id == sessionStorage.getItem("id") && (
+            <FilterButton onSelect={setFilterValue} />
+          )}
         </div>
         <CardsList id={userDetails._id} itemType={filterValue} />
       </div>
